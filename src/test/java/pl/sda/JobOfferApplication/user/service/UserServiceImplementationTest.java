b2e7e7ada28cb61fe352b65829d1dc4bf9d639ef
Uzytkownik <uzytkownik@kursGIT.pl>
2020-08-23 09:05:56 +0200

    1st amendment

--- src/test/java/pl/sda/JobOfferApplication/user/service/UserServiceImplementationTest.java
+++ src/test/java/pl/sda/JobOfferApplication/user/service/UserServiceImplementationTest.java
@@ -42,8 +42,33 @@ class UserServiceImplementationTest {
         UserOutput output = createdUsers.get(0).convertEntityToOutput();
         input.equals(output);
 
+
     }
 
+    @Test
+    public void passwordValidation() throws UserException, PasswordException {
 
+        try{
+            UserInput input = new UserInput("SDAblabla", "superuser","SDA");
+            userService.createUser(input);
+            String usedPassword = input.getPassword();
+            //UserServiceImplementation usi = new UserServiceImplementation(); instancja - odwołanie do metod z userServiceImplementation
+        } catch (UserException e) {
+            System.out.println(e.getMessage());
+            //"Please provivide stronger password: length higher than 8, one big letter, one small letter, one digit, one special character"
+        }
+    }
 
+    @Test
+    public void userInputValidation() throws UserException, PasswordException{
+        try{
+            UserInput input = new UserInput("SDAblabla", "superuser","SDAascdsvf1@#$");
+            UserInput input2 = new UserInput("SDAblabla", "gyfrw","njl1W223!@");
+            userService.createUser(input);
+            userService.createUser(input2);
+        } catch (UserException e) {
+            System.out.println(e.getMessage());
+            //"User with this login, already exists"
+        }
+    }
 }
